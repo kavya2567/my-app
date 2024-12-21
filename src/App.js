8145c1556936258b670f8f52be44145bdd05a6ef
kavya mallela <kavyamallelachowdary@gmail.com>
2024-12-21 10:34:37 +0530

    adding all the files to the github

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Apple from './Apple';
+import Orange from './Orange';
+function App() {
+  return (
+    <div className="App">
+      <Apple name="Kavya"/>
+      <Orange />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Orange.jsx
@@ -0,0 +1,8 @@
+function Orange()
+{
+    return(
+        <h1>Hi This is orange</h1>
+    )
+}
+
+export default Orange;
